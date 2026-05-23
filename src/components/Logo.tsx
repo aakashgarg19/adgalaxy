023d3f23c5ba4661a554f79fa21d0605f2edb554
Aakash Garg <aakashgarg19@gmail.com>
2026-05-23 21:13:06 +0530

    changed logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,7 +8,7 @@ export default function Logo({ className = "" }: Props) {
       className={`inline-flex items-center ${className}`}
     >
       <img
-        src="/photos/logo.jpeg"
+        src="/photos/logo.png"
         alt="Arcade Digital Galaxy"
         className="h-12 w-auto sm:h-14 select-none"
         draggable={false}
